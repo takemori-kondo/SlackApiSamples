0ef7e035d73be57f6cd159e0248d91e004923c22
HOME-20230701\Ether <etherealgarden@msn.com>
2024-05-30 17:58:22 +0900

    パラメータのやり取りに対応

--- event-sample-02/slack-sample-workflow.php
+++ event-sample-02/slack-sample-workflow.php
@@ -9,7 +9,55 @@ function slack_sample_workflow_workflow_step_edit($SLACK_AUTHZ_TOKEN, $requestDt
       {
         type: "section",
         block_id: "description",
-        text: { type:"plain_text", text:"アンケート結果をユーザのカスタムフィールドに書き込みます。" }
+        text: { type:"plain_text", text:"各項目に対応する「Insert a variable」変数を設定してください" }
+      },
+      {
+        "type": "input",
+        "element": {
+          "type": "plain_text_input",
+          "action_id": "submission_timestamp"
+        },
+        "label": {
+          "type": "plain_text",
+          "text": "Submission Timestamp",
+          "emoji": false
+        }
+      },
+      {
+        "type": "input",
+        "element": {
+          "type": "plain_text_input",
+          "action_id": "submitter_slack_email"
+        },
+        "label": {
+          "type": "plain_text",
+          "text": "Submitter Slack Email",
+          "emoji": false
+        }
+      },
+      {
+        "type": "input",
+        "element": {
+          "type": "plain_text_input",
+          "action_id": "slack_full_name"
+        },
+        "label": {
+          "type": "plain_text",
+          "text": "Slack Full Name",
+          "emoji": false
+        }
+      },
+      {
+        "type": "input",
+        "element": {
+          "type": "plain_text_input",
+          "action_id": "answer"
+        },
+        "label": {
+          "type": "plain_text",
+          "text": "Answer",
+          "emoji": false
+        }
       }
     ]
 }
@@ -32,13 +80,21 @@ HEREDOC;
 }
 
 function slack_sample_workflow_save($SLACK_AUTHZ_TOKEN, $requestDto, $payload) {
-    $inputs = '{ taskName: { value: "タスク名" }, taskDescription: { value: "タスク説明" } }';
-    $outputs =  <<<'HEREDOC'
-[
-    { type: "text", name: "taskName", label: "Task name" },
-    { type: "text", name: "taskDescription", label: "Task description" }
-]
+    // $payloadはviewプロパティの中身
+    $keyList = array_keys( $payload['state']['values']);
+    $var1 = $payload['state']['values'][$keyList[0]]['submission_timestamp']['value'];
+    $var2 = $payload['state']['values'][$keyList[1]]['submitter_slack_email']['value'];
+    $var3 = $payload['state']['values'][$keyList[2]]['slack_full_name']['value'];
+    $var4 = $payload['state']['values'][$keyList[3]]['answer']['value'];
+    $inputs = <<<"HEREDOC"
+{
+    submission_timestamp:  { "skip_variable_replacement": false, "value": "{$var1}" },
+    submitter_slack_email: { "skip_variable_replacement": false, "value": "{$var2}" },
+    slack_full_name:       { "skip_variable_replacement": false, "value": "{$var3}" },
+    answer:                { "skip_variable_replacement": false, "value": "{$var4}" }
+}
 HEREDOC;
+    $outputs = "[]";
     $url = 'https://slack.com/api/workflows.updateStep';
     $data = array(
         'token' => $SLACK_AUTHZ_TOKEN,
